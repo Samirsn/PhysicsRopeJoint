98b5f44abc600f02f421b8d5f7fbc197604b38b8
Samir <samirnaimi@gmail.com>
2014-03-17 18:47:12 -0400

    Samir & Edgar's Game
    Balloons Prototype

--- 4phyiscsBodies-master/4physicsBodys_Linked_By_RopeJoint/MyScene.m
+++ 4phyiscsBodies-master/4physicsBodys_Linked_By_RopeJoint/MyScene.m
@@ -10,7 +10,7 @@
 #import "MyScene.h"
 
 @interface MyScene()
-@property SKSpriteNode* mySquare1;
+@property SKSpriteNode* myCircle;
 @property SKSpriteNode* mySquare2;
 @property SKSpriteNode* mySquare3;
 @property SKSpriteNode* mySquare4;
@@ -19,82 +19,125 @@
 @property SKSpriteNode* mySquare7;
 @property SKSpriteNode* mySquare8;
 
-@property SKSpriteNode* myShelf;
-@property SKSpriteNode* myShelf1;
 
-@property SKPhysicsJoint* myRopeJoint;
-@property SKPhysicsJoint* myRopeJoint1;
-@property SKPhysicsJoint* myRopeJoint2;
-@property SKPhysicsJoint* myRopeJoint3;
-@property SKPhysicsJoint* myRopeJoint4;
-@property SKPhysicsJoint* myRopeJoint5;
-@property SKPhysicsJoint* myRopeJoint6;
+@property SKSpriteNode* myWallLeft;
+@property SKSpriteNode* myWallR;
+@property SKSpriteNode* myWallB;
+
+
+@property SKSpriteNode* myAvoid;
+
+@property SKSpriteNode* myAvoid1;
+
+
+//@property SKSpriteNode* myShelf;
+//@property SKSpriteNode* myShelf1;
+
+@property SKPhysicsJointPin* myRopeJoint;
+@property SKPhysicsJointPin* myRopeJoint1;
+@property SKPhysicsJointPin* myRopeJoint2;
+@property SKPhysicsJointPin* myRopeJoint3;
+@property SKPhysicsJointPin* myRopeJoint4;
+@property SKPhysicsJointPin* myRopeJoint5;
+@property SKPhysicsJointPin* myRopeJoint6;
 @end
 
 @implementation MyScene
 
 -(void) activateJointRope{
     
-    _myRopeJoint = [SKPhysicsJointLimit jointWithBodyA:_mySquare1.physicsBody bodyB:_mySquare2.physicsBody anchorA:_mySquare1.position anchorB:_mySquare2.position];
     
+    _myRopeJoint = [SKPhysicsJointPin jointWithBodyA:_myCircle.physicsBody bodyB:_mySquare2.physicsBody anchor:_myCircle.position];
     
     [self.physicsWorld addJoint:_myRopeJoint];
     
-    _myRopeJoint1 = [SKPhysicsJointLimit jointWithBodyA:_mySquare2.physicsBody bodyB:_mySquare3.physicsBody anchorA:_mySquare2.position anchorB:_mySquare3.position];
+    _myRopeJoint1 = [SKPhysicsJointPin jointWithBodyA:_mySquare2.physicsBody bodyB:_mySquare3.physicsBody anchor:_mySquare2.position];
     
     [self.physicsWorld addJoint:_myRopeJoint1];
     
-    
-    _myRopeJoint2 = [SKPhysicsJointLimit jointWithBodyA:_mySquare3.physicsBody bodyB:_mySquare4.physicsBody anchorA:_mySquare3.position anchorB:_mySquare4.position];
+    _myRopeJoint2 = [SKPhysicsJointPin jointWithBodyA:_mySquare3.physicsBody bodyB:_mySquare4.physicsBody anchor:_mySquare3.position];
     
     [self.physicsWorld addJoint:_myRopeJoint2];
     
-    
-    _myRopeJoint3 = [SKPhysicsJointLimit jointWithBodyA:_mySquare4.physicsBody bodyB:_mySquare5.physicsBody anchorA:_mySquare4.position anchorB:_mySquare5.position];
+    _myRopeJoint3 = [SKPhysicsJointPin jointWithBodyA:_mySquare4.physicsBody bodyB:_mySquare5.physicsBody anchor:_mySquare4.position];
     
     [self.physicsWorld addJoint:_myRopeJoint3];
     
-    _myRopeJoint4 = [SKPhysicsJointLimit jointWithBodyA:_mySquare5.physicsBody bodyB:_mySquare6.physicsBody anchorA:_mySquare5.position anchorB:_mySquare6.position];
+    _myRopeJoint4 = [SKPhysicsJointPin jointWithBodyA:_mySquare5.physicsBody bodyB:_mySquare6.physicsBody anchor:_mySquare5.position];
     
     [self.physicsWorld addJoint:_myRopeJoint4];
-   
-    _myRopeJoint5 = [SKPhysicsJointLimit jointWithBodyA:_mySquare6.physicsBody bodyB:_mySquare7.physicsBody anchorA:_mySquare6.position anchorB:_mySquare6.position];
+    
+    _myRopeJoint5 = [SKPhysicsJointPin jointWithBodyA:_mySquare6.physicsBody bodyB:_mySquare7.physicsBody anchor:_mySquare6.position];
     
     [self.physicsWorld addJoint:_myRopeJoint5];
     
-    _myRopeJoint6 = [SKPhysicsJointLimit jointWithBodyA:_mySquare7.physicsBody bodyB:_mySquare8.physicsBody anchorA:_mySquare7.position anchorB:_mySquare8.position];
+    _myRopeJoint6 = [SKPhysicsJointPin jointWithBodyA:_mySquare7.physicsBody bodyB:_mySquare8.physicsBody anchor:_mySquare7.position];
     
     [self.physicsWorld addJoint:_myRopeJoint6];
+    
+    
+    
+//    _myRopeJoint = [SKPhysicsJointLimit jointWithBodyA:_mySquare1.physicsBody bodyB:_mySquare2.physicsBody anchorA:_mySquare1.position anchorB:_mySquare2.position];
+//    
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint];
+//    
+//    _myRopeJoint1 = [SKPhysicsJointLimit jointWithBodyA:_mySquare2.physicsBody bodyB:_mySquare3.physicsBody anchorA:_mySquare2.position anchorB:_mySquare3.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint1];
+//    
+//    
+//    _myRopeJoint2 = [SKPhysicsJointLimit jointWithBodyA:_mySquare3.physicsBody bodyB:_mySquare4.physicsBody anchorA:_mySquare3.position anchorB:_mySquare4.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint2];
+//    
+//    
+//    _myRopeJoint3 = [SKPhysicsJointLimit jointWithBodyA:_mySquare4.physicsBody bodyB:_mySquare5.physicsBody anchorA:_mySquare4.position anchorB:_mySquare5.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint3];
+//    
+//    _myRopeJoint4 = [SKPhysicsJointLimit jointWithBodyA:_mySquare5.physicsBody bodyB:_mySquare6.physicsBody anchorA:_mySquare5.position anchorB:_mySquare6.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint4];
+//   
+//    _myRopeJoint5 = [SKPhysicsJointLimit jointWithBodyA:_mySquare6.physicsBody bodyB:_mySquare7.physicsBody anchorA:_mySquare6.position anchorB:_mySquare6.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint5];
+//    
+//    _myRopeJoint6 = [SKPhysicsJointLimit jointWithBodyA:_mySquare7.physicsBody bodyB:_mySquare8.physicsBody anchorA:_mySquare7.position anchorB:_mySquare8.position];
+//    
+//    [self.physicsWorld addJoint:_myRopeJoint6];
 }
 
 
 -(void) spawnSquares{
-    _mySquare1 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(25, 25)];
-    _mySquare1.alpha =0.8;
 
-    _mySquare2 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(21, 21)];
+    _myCircle = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(25, 25)];
+    _myCircle.alpha =0.8;
+
+    _mySquare2 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3, 15)];
     _mySquare2.alpha =0.7;
 
-    _mySquare3 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(20, 20)];
+    _mySquare3 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3, 15)];
     _mySquare3.alpha =.6;
 
-    _mySquare4 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(19, 19)];
+    _mySquare4 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3, 15)];
     _mySquare4.alpha =0.5;
 
-    _mySquare5 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(18,18)];
+    _mySquare5 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3,15)];
     _mySquare5.alpha =0.4;
 
-    _mySquare6 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(17,17)];
+    _mySquare6 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3,15)];
     _mySquare6.alpha =0.3;
 
-    _mySquare7 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(16, 16)];
+    _mySquare7 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3 ,15)];
     _mySquare7.alpha =0.2;
 
-    _mySquare8 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(15, 15)];
+    _mySquare8 =[[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(3, 15)];
     _mySquare8.alpha =0.1;
 
     
-    [_mySquare1 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5)];
+    [_myCircle setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.5)];
     [_mySquare2 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.6)];
     [_mySquare3 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.7)];
     [_mySquare4 setPosition:CGPointMake(self.size.width/1.5, self.size.height/1.8)];
@@ -104,7 +147,7 @@
     [_mySquare8 setPosition:CGPointMake(self.size.width/1.5, self.size.height/2.2)];
     //[_mySquare8 setPosition:CGPointMake(200, 203)];
     
-    _mySquare1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare1.size];
+    _myCircle.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:30];
     _mySquare2.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare2.size];
     _mySquare3.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare3.size];
     _mySquare4.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare4.size];
@@ -114,17 +157,17 @@
     _mySquare8.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_mySquare8.size];
     //_mySquare8.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:1];
     
-    [_mySquare1.physicsBody setRestitution:1.4];
-    [_mySquare2.physicsBody setRestitution:1.0];
-    [_mySquare3.physicsBody setRestitution:1.0];
-    [_mySquare4.physicsBody setRestitution:1.0];
-    [_mySquare5.physicsBody setRestitution:1.0];
-    [_mySquare6.physicsBody setRestitution:1.0];
-    [_mySquare7.physicsBody setRestitution:1.0];
-    [_mySquare8.physicsBody setRestitution:1.0];
+    [_myCircle.physicsBody setRestitution:.5];
+    [_mySquare2.physicsBody setRestitution:0];
+    [_mySquare3.physicsBody setRestitution:0];
+    [_mySquare4.physicsBody setRestitution:0];
+    [_mySquare5.physicsBody setRestitution:0];
+    [_mySquare6.physicsBody setRestitution:0];
+    [_mySquare7.physicsBody setRestitution:0];
+    [_mySquare8.physicsBody setRestitution:0];
     
-    
-    [self addChild:_mySquare1];
+
+    [self addChild:_myCircle];
     [self addChild:_mySquare2];
     [self addChild:_mySquare3];
     [self addChild:_mySquare4];
@@ -133,40 +176,93 @@
     [self addChild:_mySquare7];
     [self addChild:_mySquare8];
     
+    
+   
+    
 }
 
--(void)makeShelf{
-    _myShelf = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(50, 20)];
-    _myShelf.position = CGPointMake(100, 400);
-    _myShelf.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf.size];
-    [_myShelf.physicsBody setDynamic:NO];
+//-(void)makeShelf{
+//    _myShelf = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(50, 20)];
+//    _myShelf.position = CGPointMake(100, 400);
+//    _myShelf.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf.size];
+//    [_myShelf.physicsBody setDynamic:NO];
+//    
+//    [self addChild:_myShelf];
+//    
+//    _myShelf1 = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(50, 20)];
+//    _myShelf1.position = CGPointMake(100, 300);
+//    _myShelf1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf1.size];
+//    [_myShelf1.physicsBody setDynamic:NO];
+//    
+//    [self addChild:_myShelf1];
+//    
+//    
+//}
+
+
+
+-(void)makeAvoid{
+    _myAvoid = [[SKSpriteNode alloc]initWithColor:[SKColor yellowColor] size:CGSizeMake(50, 50)];
+    _myAvoid.position = CGPointMake(100, 300);
+    _myAvoid.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myAvoid.size];
+    [_myAvoid.physicsBody setDynamic:YES];
+
+    [self addChild:_myAvoid];
     
-    [self addChild:_myShelf];
     
-    _myShelf1 = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(50, 20)];
-    _myShelf1.position = CGPointMake(100, 300);
-    _myShelf1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myShelf1.size];
-    [_myShelf1.physicsBody setDynamic:NO];
+    _myAvoid1 = [[SKSpriteNode alloc]initWithColor:[SKColor redColor] size:CGSizeMake(50, 50)];
+    _myAvoid1.position = CGPointMake(100, 100);
+    _myAvoid1.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myAvoid1.size];
+    [_myAvoid1.physicsBody setDynamic:YES];
     
-    [self addChild:_myShelf1];
-    
-    
+    [self addChild:_myAvoid1];
+
 }
+-(void)makeWall{
+        _myWallLeft = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(10, 1150)];
+        _myWallLeft.position = CGPointMake(10, 0);
+        _myWallLeft.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myWallLeft.size];
+        [_myWallLeft.physicsBody setDynamic:NO];
+    
+        [self addChild:_myWallLeft];
+    
+    _myWallR = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(10, 1150)];
+    _myWallR.position = CGPointMake(315, 0);
+    _myWallR.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myWallR.size];
+    [_myWallR.physicsBody setDynamic:NO];
+    
+    [self addChild:_myWallR];
+    
+//    _myWallB = [[SKSpriteNode alloc]initWithColor:[SKColor lightGrayColor] size:CGSizeMake(250, 10)];
+//    _myWallB.position = CGPointMake(225, 200);
+//    _myWallB.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:_myWallR.size];
+//    [_myWallB.physicsBody setDynamic:NO];
+//    
+//    [self addChild:_myWallB];
+    }
+
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         /* Setup your scene here */
         
         self.scaleMode = SKSceneScaleModeAspectFit;
-        self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
+       // self.physicsBody = [SKPhysicsBody bodyWithEdgeLoopFromRect:self.frame];
         [self.physicsBody setRestitution:1];
         
         
         [self spawnSquares];
         
         [self activateJointRope];
-       [self makeShelf];
+//       [self makeShelf];
+     
         
+        [self makeWall];
+        
+        [self makeAvoid];
+        
+        self.physicsWorld.gravity = CGVectorMake(0, 1);
+
     }
     return self;
 }
@@ -174,21 +270,21 @@
 -(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     /* Called when a touch begins */
     
-    if (_mySquare1.physicsBody.dynamic) {
+    if (_mySquare8.physicsBody.dynamic) {
         
-        [_mySquare1.physicsBody setDynamic:NO];
-        
-        
+        [_mySquare8.physicsBody setDynamic:NO];
         
         
-        _mySquare1.color = [SKColor blueColor];
-        _mySquare2.color = [SKColor blueColor];
-        _mySquare3.color = [SKColor blueColor];
-        _mySquare4.color = [SKColor blueColor];
-        _mySquare5.color = [SKColor blueColor];
-        _mySquare6.color = [SKColor blueColor];
-        _mySquare7.color = [SKColor blueColor];
-        _mySquare8.color = [SKColor blueColor];
+        
+        
+        _myCircle.color = [SKColor blueColor];
+        _mySquare2.color = [SKColor grayColor];
+        _mySquare3.color = [SKColor grayColor];
+        _mySquare4.color = [SKColor grayColor];
+        _mySquare5.color = [SKColor grayColor];
+        _mySquare6.color = [SKColor grayColor];
+        _mySquare7.color = [SKColor grayColor];
+        _mySquare8.color = [SKColor grayColor];
 
 
         
@@ -196,7 +292,7 @@
     
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        [_mySquare1 setPosition:location];
+        [_myCircle setPosition:location];
         
         
         
@@ -206,40 +302,42 @@
         
         
         
+        
     }
+
 }
 
 -(void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
     for (UITouch *touch in touches) {
         CGPoint location = [touch locationInNode:self];
-        [_mySquare1 setPosition:location];
+        [_mySquare8 setPosition:location];
         
     }
 }
 
 -(void) touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event  {
     
-    if (!_mySquare1.physicsBody.dynamic) {
-        [_mySquare1.physicsBody setDynamic:YES];
+    if (!_mySquare8.physicsBody.dynamic) {
+        [_mySquare8.physicsBody setDynamic:YES];
     }
     //  [_mySquare1.physicsBody setDynamic:YES];
     //  [_mySquare2.physicsBody setDynamic:YES];
-    _mySquare1.color = [SKColor redColor];
-    _mySquare2.color = [SKColor redColor];
-    _mySquare3.color = [SKColor redColor];
-    _mySquare4.color = [SKColor redColor];
-    _mySquare5.color = [SKColor redColor];
-    _mySquare6.color = [SKColor redColor];
-    _mySquare7.color = [SKColor redColor];
-    _mySquare8.color = [SKColor redColor];
+    _myCircle.color = [SKColor greenColor];
+    _mySquare2.color = [SKColor grayColor];
+    _mySquare3.color = [SKColor grayColor];
+    _mySquare4.color = [SKColor grayColor];
+    _mySquare5.color = [SKColor grayColor];
+    _mySquare6.color = [SKColor grayColor];
+    _mySquare7.color = [SKColor grayColor];
+    _mySquare8.color = [SKColor grayColor];
     
     
 }
 
 -(void) touchesCancelled:(NSSet *)touches withEvent:(UIEvent *)event{
     
-    if (!_mySquare1.physicsBody.dynamic) {
-        [_mySquare1.physicsBody setDynamic:YES];
+    if (!_mySquare8.physicsBody.dynamic) {
+        [_mySquare8.physicsBody setDynamic:YES];
     }
     //  [_mySquare1.physicsBody setDynamic:YES];
     //  [_mySquare2.physicsBody setDynamic:YES];
@@ -250,6 +348,20 @@
 
 -(void)update:(CFTimeInterval)currentTime {
     /* Called before each frame is rendered */
+    
+    //FOR THE AVOID SQUARE
+    if (_myAvoid.position.y > 550) {
+        _myAvoid.position = CGPointMake(200, -30);
+        _myAvoid.physicsBody.velocity = CGVectorMake(0, 0);
+    }
+    
+    
+    //FOR THE AVOID SQUARE
+    if (_myAvoid1.position.y > 550) {
+        _myAvoid1.position = CGPointMake(80, -30);
+        _myAvoid1.physicsBody.velocity = CGVectorMake(0, 0);
+    }
+
 }
 
 @end
